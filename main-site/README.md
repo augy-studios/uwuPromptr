7caa03e331a76b9c88d806db0d74141275b6e682
Augy <ateoks@gmail.com>
2026-09-19 22:10:18 +0800

    initial structure

--- main-site/README.md
+++ main-site/README.md
@@ -1,3 +1,3 @@
-# pwa-template
-Augy Studios PWA sites template
+# uwuPromptr
+Augy Studios PWA sites uwuPromptr
 Note: The `/api` folder is meant for Vercel serverless functions. Remove if not required.
--- main-site/sw.js
+++ main-site/sw.js
@@ -1,12 +1,12 @@
-const CACHE = "template-offline-v1";
+const CACHE = "uwuPromptr-v1";
 
 const ASSETS = [
   "/",
   "/index.html",
   "/style.css",
   "/script.js",
-  "/templateicon1-192.png",
-  "/templateicon1-512.png",
+  "/UPC-192.png",
+  "/UPC-512.png",
   "/favicon.ico",
   "/manifest.json"
 ];
